10f81d37c4b85f656e6ef5b319fa0cfbb0213668
Yossia Dwi Mahardika <yossia.dwi@ti.ukdw.ac.id>
2025-03-15 17:06:59 +0700

    Initial commit

---
+++ src/main/java/com/example/belajar_spring_async/BelajarSpringAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/belajar_spring_async/BelajarSpringAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.belajar_spring_async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
